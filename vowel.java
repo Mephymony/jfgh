d711064fac977ac1f54564f233aa7ee17ae32e64
Mephymony <mebinmony@gmail.com>
2017-07-11 12:25:39 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char i=sc.nextLine();
+if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u')
+{
+System.out.println("Vowel");
+}
+else
+{
+System.out.println("Consonant");
+}
+}
+}
